49fc7a26c4eab120658c351a985336ffc922fece
sthemeli <skthemeli@gmail.com>
2020-11-29 18:20:52 +0200

    optional validation + test sets

--- models/training.py
+++ models/training.py
@@ -14,7 +14,7 @@ from models.relations_model import RelationsClassifier
 
 class ArgumentMiningTrainer:
 
-    def __init__(self, app_config, data, labels, lbl_dict):
+    def __init__(self, app_config, data, labels, lbl_dict, test=False, val=False):
         self.app_config = app_config
         self.app_logger = app_config.app_logger
         self.properties = app_config.properties
@@ -24,24 +24,33 @@ class ArgumentMiningTrainer:
         self.data = data
         self.labels = labels
         self.lbl_dict = lbl_dict
-        self.train_data, self.test_data, self.train_labels, self.test_labels = train_test_split(data, labels,
-                                                                                                test_size=test_size,
-                                                                                                random_state=0)
-        k_fold = KFold(n_splits=self.num_folds)
-        self.folds = []
-        self.classifiers = []
-        self.trainers = []
-        for train_index, test_index in k_fold.split(self.train_data, self.train_labels):
-            x_train_fold = self.train_data[train_index]
-            y_train_fold = self.train_labels[train_index]
-            x_validation_fold = self.train_data[test_index]
-            y_validation_fold = self.train_labels[test_index]
-            fold = {"train_data": x_train_fold, "train_labels": y_train_fold, "validation_data": x_validation_fold,
-                    "validation_labels": y_validation_fold}
+        if test:
+            self.train_data, self.test_data, self.train_labels, self.test_labels = train_test_split(data, labels,
+                                                                                                    test_size=test_size,
+                                                                                                    random_state=0)
+        else:
+            self.train_data, self.test_data, self.train_labels, self.test_labels = data, data, labels, labels
+        if val:
+            k_fold = KFold(n_splits=self.num_folds)
+            self.folds = []
+            self.classifiers = []
+            self.trainers = []
+            for train_index, test_index in k_fold.split(self.train_data, self.train_labels):
+                x_train_fold = self.train_data[train_index]
+                y_train_fold = self.train_labels[train_index]
+                x_validation_fold = self.train_data[test_index]
+                y_validation_fold = self.train_labels[test_index]
+                fold = {"train_data": x_train_fold, "train_labels": y_train_fold, "validation_data": x_validation_fold,
+                        "validation_labels": y_validation_fold}
+                self.folds.append(fold)
+        else:
+            fold = {"train_data": self.train_data, "train_labels": self.train_labels, "validation_data": self.train_data,
+                    "validation_labels": self.train_labels}
             self.folds.append(fold)
 
     def train(self, kind="adu"):
         fold_counter = 0
+
         for fold in self.folds:
             fold_counter += 1
             self.app_logger.debug("Fold counter: {}".format(fold_counter))
