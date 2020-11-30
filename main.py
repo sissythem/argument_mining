bb2118fc6934bd6576002a6fb80903ebe890277e
sthemeli <c.themeli@gmail.com>
2020-11-30 17:50:53 +0200

    update main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from data.adu_data import DataPreprocessor as adudp
 from data.relations_data import DataPreprocessor as reldp
 from data.training_data import DataLoader
 from models.training import ArgumentMiningTrainer
+from models.arg_mining_pipeline import ArgumentMining
 
 
 def load_documents(logger, app_config):
@@ -59,21 +60,35 @@ def main():
     app_config = AppConfig(app_path=app_path)
     app_config.configure()
     logger = app_config.app_logger
+    tasks = app_config.properties["tasks"]
+    try:
+        adu_model_file, relations_model_file, stance_model_file = app_config.properties["classifier_files"][0], \
+                                                                  app_config.properties["classifier_files"][1], \
+                                                                  app_config.properties["classifier_files"][2]
+    except (KeyError, Exception):
+        adu_model_file, relations_model_file, stance_model_file = "", "", ""
     try:
         documents = load_documents(logger=logger, app_config=app_config)
-        sentences, labels, lbl_dict = adu_preprocessing(documents=documents, logger=logger, app_config=app_config)
-        adu_model_file = adu_training(sentences=sentences, labels=labels, lbl_dict=lbl_dict, app_config=app_config)
+        if "train" in tasks:
+            sentences, labels, lbl_dict = adu_preprocessing(documents=documents, logger=logger, app_config=app_config)
+            adu_model_file = adu_training(sentences=sentences, labels=labels, lbl_dict=lbl_dict, app_config=app_config)
 
-        logger.info("Preprocessing relations data")
-        rel_sentences, rel_labels, rel_lbl_dict = relations_preprocessing(documents=documents, app_config=app_config)
-        logger.info("Preprocessing stance data")
-        stance_sentences, stance_lbls, stance_lbl_dict = relations_preprocessing(documents=documents,
-                                                                                 app_config=app_config, kind="stance")
-        relations_model_file = relations_training(app_config=app_config, data=rel_sentences, labels=rel_labels,
-                                                  lbl_dict=rel_lbl_dict)
-        stance_model_file = relations_training(app_config=app_config, data=stance_sentences, labels=stance_lbls,
-                                               lbl_dict=stance_lbl_dict)
-
+            logger.info("Preprocessing relations data")
+            rel_sentences, rel_labels, rel_lbl_dict = relations_preprocessing(documents=documents,
+                                                                              app_config=app_config)
+            logger.info("Preprocessing stance data")
+            stance_sentences, stance_lbls, stance_lbl_dict = relations_preprocessing(documents=documents,
+                                                                                     app_config=app_config,
+                                                                                     kind="stance")
+            relations_model_file = relations_training(app_config=app_config, data=rel_sentences, labels=rel_labels,
+                                                      lbl_dict=rel_lbl_dict)
+            stance_model_file = relations_training(app_config=app_config, data=stance_sentences, labels=stance_lbls,
+                                                   lbl_dict=stance_lbl_dict)
+        if "exec" in tasks:
+            arg_mining = ArgumentMining(app_config=app_config)
+            arg_mining.load(adu_model_file_path=adu_model_file, relations_model_file_path=relations_model_file,
+                            stance_model_file_path=stance_model_file)
+            arg_mining.predict(documents=documents)
     except(Exception, BaseException) as e:
         logger.error("Error occurred: {}".format(traceback.format_exc()))
         app_config.send_email(body="An error occurred during the run!{}".format(traceback.format_exc()))
