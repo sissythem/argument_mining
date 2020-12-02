d17bcdc9d87b70ccec065a8b1a2dddcc4b82a50c
sthemeli <c.themeli@gmail.com>
2020-12-02 15:45:34 +0200

    fix device

--- base/utils.py
+++ base/utils.py
@@ -13,7 +13,7 @@ def wrap_and_pad_tokens(inputs, prefix, suffix, seq_len, padding, pad=True, trun
     out = [prefix] + inputs
     if truncate:
         out = out[:seq_len - 1]
+    out = out + [suffix]
     if pad:
         out += [padding] * (seq_len - 1 - len(out))
-    out += [suffix]
     return out
