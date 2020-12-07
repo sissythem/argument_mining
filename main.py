d8c2ae75e626491320bbee418d3cf4a75a2461e6
sthemeli <c.themeli@gmail.com>
2020-12-07 14:26:36 +0200

    small update

--- main.py
+++ main.py
@@ -71,15 +71,15 @@ def main():
         documents = load_documents(logger=logger, app_config=app_config)
         if "train" in tasks:
             sentences, labels, lbl_dict = adu_preprocessing(documents=documents, logger=logger, app_config=app_config)
-            # adu_model_file = adu_training(sentences=sentences, labels=labels, lbl_dict=lbl_dict, app_config=app_config)
+            adu_model_file = adu_training(sentences=sentences, labels=labels, lbl_dict=lbl_dict, app_config=app_config)
             # app_config.send_email(body="Finished ADU model training")
             logger.info("Preprocessing relations data")
-            rel_sentences, rel_labels, rel_lbl_dict = relations_preprocessing(documents=documents,
-                                                                              app_config=app_config)
+            # rel_sentences, rel_labels, rel_lbl_dict = relations_preprocessing(documents=documents,
+            #                                                                   app_config=app_config)
             logger.info("Preprocessing stance data")
-            stance_sentences, stance_lbls, stance_lbl_dict = relations_preprocessing(documents=documents,
-                                                                                     app_config=app_config,
-                                                                                     kind="stance")
+            # stance_sentences, stance_lbls, stance_lbl_dict = relations_preprocessing(documents=documents,
+            #                                                                          app_config=app_config,
+            #                                                                          kind="stance")
             # relations_model_file = relations_training(app_config=app_config, data=rel_sentences, labels=rel_labels,
             #                                          lbl_dict=rel_lbl_dict)
             # app_config.send_email(body="Finished relations model training")
