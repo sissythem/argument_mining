8130ba83b532bf277ed82c0025f338fe74432f93
sthemeli <c.themeli@gmail.com>
2020-12-08 23:12:37 +0200

    refactor flair

--- base/config.py
+++ base/config.py
@@ -7,41 +7,30 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from os import mkdir, environ
+from os import mkdir
 from os.path import exists, join
-import torch
+
 import yaml
+from torch.optim.adam import Adam
+
+from base import utils
 
 
-class AppConfig:
-    properties_file = "properties.yaml"
-    example_properties = "example_properties.yaml"
-    documents_pickle = "documents.pkl"
-    pickle_sentences_filename = "sentences.pkl"
-    pickle_segments_filename = "segments.pkl"
-    relations_pickle_file = "relations.pkl"
-    stances_pickle_file = "stances.pkl"
-    pickle_relations_labels = "relations_labels.pkl"
-    log_filename = 'logs_%s' % datetime.now().strftime('%Y%m%d-%H%M%S')
+class Config:
 
-    def __init__(self, app_path):
-        # paths & directories
+    def __init__(self, app_path, properties_file, example_properties):
+        self.log_filename = 'logs_%s' % datetime.now().strftime('%Y%m%d-%H%M%S')
+        self.documents_pickle = "documents.pkl"
+        self.properties_file = properties_file
+        self.example_properties = example_properties
         self.app_path = app_path
         self.run = uuid.uuid4().hex
         self._create_paths()
         self._create_output_dirs()
+        self.device_name = utils.configure_device()
         self.properties = {}
-        self.data_file = ""
         self.app_logger = None
-        if torch.cuda.is_available():
-            devices = environ.get("CUDA_VISIBLE_DEVICES", 0)
-            if type(devices) == str:
-                devices = devices.split(",")
-                self.device_name = "cuda:{}".format(devices[0].strip())
-            else:
-                self.device_name = "cuda:{}".format(devices)
-        else:
-            self.device_name = "cpu"
+        self.data_file = None
 
     def configure(self):
         # logging
@@ -51,32 +40,7 @@ class AppConfig:
         # properties
         self.properties = self._load_properties()
         config = self.properties["config"]
-        self.data_file = config["data"]["filename"]
         self._config_email(config=config)
-
-    def _create_paths(self):
-        self.resources_path = join(self.app_path, "resources")
-        self.output_path = join(self.app_path, "output")
-        self.logs_path = join(self.output_path, "logs")
-        self.model_path = join(self.output_path, "model")
-        self.tensorboard_path = join(self.output_path, "runs")
-        self.out_files_path = join(self.output_path, "output_files")
-
-    def _create_output_dirs(self):
-        if not exists(self.output_path):
-            mkdir(self.output_path)
-        if not exists(self.out_files_path):
-            mkdir(self.out_files_path)
-        if not exists(self.logs_path):
-            mkdir(self.logs_path)
-        if not exists(self.tensorboard_path):
-            mkdir(self.tensorboard_path)
-        if not exists(self.model_path):
-            mkdir(self.model_path)
-        if not exists(join(self.model_path, self.run)):
-            mkdir(join(self.model_path, self.run))
-        if not exists(join(self.tensorboard_path, self.run)):
-            mkdir(join(self.tensorboard_path, self.run))
 
     def _config_logger(self):
         log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
@@ -105,6 +69,30 @@ class AppConfig:
         self.sender_email = config_email.get("sender", "skthemeli@gmail.com")
         self.receiver_email = config_email.get("receiver", "skthemeli@gmail.com")
         self.password = config_email["password"]
+
+    def _create_paths(self):
+        self.resources_path = join(self.app_path, "resources")
+        self.output_path = join(self.app_path, "output")
+        self.logs_path = join(self.output_path, "logs")
+        self.model_path = join(self.output_path, "model")
+        self.tensorboard_path = join(self.output_path, "runs")
+        self.out_files_path = join(self.output_path, "output_files")
+
+    def _create_output_dirs(self):
+        if not exists(self.output_path):
+            mkdir(self.output_path)
+        if not exists(self.out_files_path):
+            mkdir(self.out_files_path)
+        if not exists(self.logs_path):
+            mkdir(self.logs_path)
+        if not exists(self.tensorboard_path):
+            mkdir(self.tensorboard_path)
+        if not exists(self.model_path):
+            mkdir(self.model_path)
+        if not exists(join(self.model_path, self.run)):
+            mkdir(join(self.model_path, self.run))
+        if not exists(join(self.tensorboard_path, self.run)):
+            mkdir(join(self.tensorboard_path, self.run))
 
     def send_email(self, body, subject=None):
         if not subject:
@@ -144,3 +132,39 @@ class AppConfig:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             server.login(self.sender_email, self.password)
             server.sendmail(self.sender_email, self.receiver_email, text)
+
+
+class FlairConfig(Config):
+    properties_file = "properties_flair.yaml"
+    example_properties = "example_properties_flair.yaml"
+
+    def __init__(self, app_path):
+        super(FlairConfig, self).__init__(app_path=app_path, properties_file=self.properties_file,
+                                          example_properties=self.example_properties)
+        self.base_path = utils.get_base_path(path=self.output_path, hidden_size=self.properties["hidden_size"],
+                                             rnn_layers=self.properties["rnn_layers"],
+                                             use_crf=self.properties["use_crf"], optimizer=Adam,
+                                             learning_rate=self.properties["learning_rate"],
+                                             mini_batch_size=self.properties["mini_batch_size"])
+
+        config = self.properties["config"]
+        self.eval_doc = config["eval_doc"]
+        self.train_csv = config["train_csv"]
+        self.dev_csv = config["dev_csv"]
+        self.test_csv = config["test_csv"]
+
+
+class AppConfig(Config):
+    properties_file = "properties.yaml"
+    example_properties = "example_properties.yaml"
+    pickle_sentences_filename = "sentences.pkl"
+    pickle_segments_filename = "segments.pkl"
+    relations_pickle_file = "relations.pkl"
+    stances_pickle_file = "stances.pkl"
+    pickle_relations_labels = "relations_labels.pkl"
+
+    def __init__(self, app_path):
+        super(AppConfig, self).__init__(app_path=app_path, properties_file=self.properties_file,
+                                        example_properties=self.example_properties)
+        config = self.properties["config"]
+        self.data_file = config["data"]["filename"]
--- main_flair.py
+++ main_flair.py
@@ -1,34 +1,24 @@
 import random
-from os.path import join
 
-from torch.optim.adam import Adam
-
-from flair_impl import preprocessing as prep
-from flair_impl import utils
-from flair_impl import flair_simple as fl
-from base.config import AppConfig
+from base import utils
+from base.config import FlairConfig
+from data import flair_preprocessing as prep
+from models.adu_model_flair import AduModel
 
 
 def main():
     random.seed(2020)
-    properties = utils.get_properties()
     curr_dir = utils.get_curr_path()
-    app_config = AppConfig(app_path=curr_dir)
-    output_path = join(curr_dir, "output")
-    data_folder = join(curr_dir, "resources")
-    base_path = utils.get_base_path(path=output_path, hidden_size=properties["hidden_size"],
-                                    rnn_layers=properties["rnn_layers"],
-                                    use_crf=properties["use_crf"], optimizer=Adam,
-                                    learning_rate=properties["learning_rate"],
-                                    mini_batch_size=properties["mini_batch_size"])
+    app_config = FlairConfig(app_path=curr_dir)
+    properties = app_config.properties
     do_prep = properties["do_prep"]
     if do_prep:
-        prep.preprocess_adus()
-    fl.adu_train(properties=properties, data_folder=data_folder, base_path=base_path)
-    model_path = join(base_path, "best-model.pt")
-    input_file_path = join(data_folder, "kasteli.json")
-    out_dir = join(output_path, "output_files")
-    fl.predict(model_path=model_path, input_file_path=input_file_path, out_dir=out_dir)
+        prep.adu_preprocess(app_config=app_config)
+    adu_model = AduModel(app_config=app_config)
+    if "train" in properties["tasks"]:
+        adu_model.train()
+    if "eval" in properties["tasks"]:
+        adu_model.predict()
 
 
 if __name__ == '__main__':
