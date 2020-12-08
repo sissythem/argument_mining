f9b4b17069fbec72720f8eabae8a3c3af977ed70
sthemeli <skthemeli@gmail.com>
2020-12-08 23:38:36 +0200

    add logs

--- data/flair_preprocessing.py
+++ data/flair_preprocessing.py
@@ -5,18 +5,22 @@ import pandas as pd
 
 
 def adu_preprocess(app_config):
+    logger = app_config.app_logger
+    logger.debug("Running ADU preprocessing")
     resources = app_config.resources_path
     documents_path = join(resources, app_config.documents_pickle)
+    logger.debug("Loading documents from pickle file")
     with open(documents_path, "rb") as f:
         documents = pickle.load(f)
-    df = pd.DataFrame(columns=["token", "label", "arg", "doc_sentence", "sentence", "doc_id"])
+    logger.debug("Documents are loaded")
+    df = pd.DataFrame()
     row_counter = 0
     sentence_counter = 0
     for document in documents:
-        print("Processing document with id: {}".format(document.document_id))
+        logger.debug("Processing document with id: {}".format(document.document_id))
         doc_sentence_counter = 0
         for idx, sentence in enumerate(document.sentences):
-            print("Processing sentence: {}".format(sentence))
+            logger.debug("Processing sentence: {}".format(sentence))
             labels = document.sentences_labels[idx]
             for token, label in zip(sentence, labels):
                 is_arg = "Y" if label != "O" else "N"
@@ -29,10 +33,10 @@ def adu_preprocess(app_config):
                 doc_sentence_counter += 1
             df.loc[row_counter] = ["", "", "", "", "", ""]
             row_counter += 1
-    print("Finished building dataframing. Saving...")
+    logger.debug("Finished building dataframing. Saving...")
     out_file_path = join(resources, "train.csv")
-    df.to_csv(out_file_path, sep='\t', index=False)
-    print("Dataframe saved!")
+    df.to_csv(out_file_path, sep='\t', index=False, header=None)
+    logger.debug("Dataframe saved!")
 
 
 def preprocess_relations():
