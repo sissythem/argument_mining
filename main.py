3e86de592c587daa108d3c82165320adb95f9843
sthemeli <skthemeli@gmail.com>
2020-12-15 10:44:25 +0200

    refactor main

--- main.py
+++ main.py
@@ -8,7 +8,38 @@ from training_data import DataLoader
 from utils import AppConfig
 
 
-def collect_documents(app_config):
+def preprocess(app_config):
+    logger = app_config.app_logger
+    data_loader = DataLoader(app_config=app_config)
+    logger.info("Creating CSV file in CONLL format for ADUs classification")
+    data_loader.load_adus()
+    logger.info("Creating CSV file in CONLL format for relations classification")
+    data_loader.load_relations()
+    logger.info("Creating CSV file in CONLL format for stance classification")
+
+
+def train(app_config):
+    logger = app_config.app_logger
+    logger.info("Training ADU classifier")
+    adu_model = AduModel(app_config=app_config)
+    adu_model.train()
+    logger.info("ADU Training is finished!")
+    logger.info("Training relations model")
+    rel_model = RelationsModel(app_config=app_config, dev_csv=app_config.rel_dev_csv,
+                               train_csv=app_config.rel_train_csv, test_csv=app_config.rel_test_csv,
+                               base_path=app_config.rel_base_path, model_name="rel")
+    rel_model.train()
+    logger.info("Relations training finished!")
+    logger.info("Training stance model")
+    stance_model = RelationsModel(app_config=app_config, dev_csv=app_config.stance_dev_csv, model_name="stance",
+                                  train_csv=app_config.stance_train_csv, test_csv=app_config.stance_test_csv,
+                                  base_path=app_config.stance_base_path)
+    stance_model.train()
+    logger.info("Stance training finished!")
+
+
+def evaluate(app_config):
+    logger = app_config.app_logger
     client = esclient_swo.elastic_server_client
     file_path = join(app_config.resources_path, "kasteli_34_urls.txt")
     # read the list of urls from the file:
@@ -25,40 +56,19 @@ def collect_documents(app_config):
         found += 1
     print(f"Found documents: {found}")
     esclient_swo.stop()
+    logger.info("Evaluation is finished!")
 
 
 def main():
     app_config: AppConfig = AppConfig()
-    logger = app_config.app_logger
     properties = app_config.properties
-    do_prep = properties["do_prep"]
-    if do_prep:
-        data_loader = DataLoader(app_config=app_config)
-        logger.info("Creating CSV file in CONLL format for ADUs classification")
-        data_loader.load_adus()
-        logger.info("Creating CSV file in CONLL format for relations classification")
-        data_loader.load_relations()
-        logger.info("Creating CSV file in CONLL format for stance classification")
-    if "train" in properties["tasks"]:
-        logger.info("Training ADU classifier")
-        adu_model = AduModel(app_config=app_config)
-        adu_model.train()
-        logger.info("ADU Training is finished!")
-        logger.info("Training relations model")
-        rel_model = RelationsModel(app_config=app_config, dev_csv=app_config.rel_dev_csv,
-                                   train_csv=app_config.rel_train_csv, test_csv=app_config.rel_test_csv,
-                                   base_path=app_config.rel_base_path, model_name="rel")
-        rel_model.train()
-        logger.info("Relations training finished!")
-        logger.info("Training stance model")
-        stance_model = RelationsModel(app_config=app_config, dev_csv=app_config.stance_dev_csv, model_name="stance",
-                                      train_csv=app_config.stance_train_csv, test_csv=app_config.stance_test_csv,
-                                      base_path=app_config.stance_base_path)
-        stance_model.train()
-        logger.info("Stance training finished!")
+    tasks = properties["tasks"]
+    if "prep" in tasks:
+        preprocess(app_config=app_config)
+    if "train" in tasks:
+        train(app_config=app_config)
     if "eval" in properties["tasks"]:
-        collect_documents(app_config=app_config)
-        logger.info("Evaluation is finished!")
+        evaluate(app_config=app_config)
 
 
 if __name__ == '__main__':
