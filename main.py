6749b84ae494f7e712e2872fead7e25661e83314
sthemeli <c.themeli@gmail.com>
2020-12-17 18:59:20 +0200

    update pipeline

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import json
 import traceback
-from os.path import join
 
-from elasticsearch_dsl import Search
-
-import utils
-from arg_mining import AduModel, RelationsModel, ArgumentMining
-from training_data import DataLoader
+from classifiers import AduModel, RelationsModel
+from pipeline import ArgumentMining
+from data import DataLoader
 from utils import AppConfig
 
 
@@ -15,77 +11,37 @@ def preprocess(app_config):
     data_loader = DataLoader(app_config=app_config)
     logger.info("Creating CSV file in CONLL format for ADUs classification")
     data_loader.load_adus()
-    logger.info("Creating CSV file in CONLL format for relations classification")
+    logger.info("Creating CSV file in CONLL format for relations/stance classification")
     data_loader.load_relations()
-    logger.info("Creating CSV file in CONLL format for stance classification")
 
 
 def train(app_config):
     logger = app_config.app_logger
-    logger.info("Training ADU classifier")
-    adu_model = AduModel(app_config=app_config)
-    adu_model.train()
-    logger.info("ADU Training is finished!")
-    logger.info("Training relations model")
-    rel_model = RelationsModel(app_config=app_config, dev_csv=app_config.rel_dev_csv,
-                               train_csv=app_config.rel_train_csv, test_csv=app_config.rel_test_csv,
-                               base_path=app_config.rel_base_path, model_name="rel")
-    rel_model.train()
-    logger.info("Relations training finished!")
-    logger.info("Training stance model")
-    stance_model = RelationsModel(app_config=app_config, dev_csv=app_config.stance_dev_csv, model_name="stance",
-                                  train_csv=app_config.stance_train_csv, test_csv=app_config.stance_test_csv,
-                                  base_path=app_config.stance_base_path)
-    stance_model.train()
-    logger.info("Stance training finished!")
+    models_to_train = app_config.properties["train"]["models"]
+    if "adu" in models_to_train:
+        logger.info("Training ADU classifier")
+        adu_model = AduModel(app_config=app_config)
+        adu_model.train()
+        logger.info("ADU Training is finished!")
+    if "rel" in models_to_train:
+        logger.info("Training relations model")
+        rel_model = RelationsModel(app_config=app_config, dev_csv=app_config.rel_dev_csv,
+                                   train_csv=app_config.rel_train_csv, test_csv=app_config.rel_test_csv,
+                                   base_path=app_config.rel_base_path, model_name="rel")
+        rel_model.train()
+        logger.info("Relations training finished!")
+    if "stance" in models_to_train:
+        logger.info("Training stance model")
+        stance_model = RelationsModel(app_config=app_config, dev_csv=app_config.stance_dev_csv, model_name="stance",
+                                      train_csv=app_config.stance_train_csv, test_csv=app_config.stance_test_csv,
+                                      base_path=app_config.stance_base_path)
+        stance_model.train()
+        logger.info("Stance training finished!")
 
 
 def evaluate(app_config):
-    logger = app_config.app_logger
-    eval_source = app_config.properties["eval"]["source"]
-    if eval_source == "elasticsearch":
-        eval_from_elasticsearch(app_config)
-    else:
-        eval_from_file(app_config=app_config)
-    logger.info("Evaluation is finished!")
-
-
-def eval_from_elasticsearch(app_config):
-    logger = app_config.app_logger
-    client = app_config.elastic_retrieve.elasticsearch_client
-    # TODO retrieve previous day's articles
-    file_path = join(app_config.resources_path, "kasteli_34_urls.txt")
-    # read the list of urls from the file:
-    with open(file_path, "r") as f:
-        urls = [line.rstrip() for line in f]
-    search_articles = Search(using=client, index='articles').filter('terms', link=urls)
-    found = 0
     arg_mining = ArgumentMining(app_config=app_config)
-    ids = []
-    for hit in search_articles.scan():
-        document = hit.to_dict()
-        document["id"] = hit.meta["id"]
-        ids.append(document["id"])
-        if not document["content"].startswith(document["title"]):
-            document["content"] = document["title"] + "\r\n\r\n" + document["content"]
-        arg_mining.predict(document=document)
-        found += 1
-    logger.info(f"Found documents: {found}")
-    logger.info("Saved ids: {}".format(ids))
-
-
-def eval_from_file(app_config, filename="kasteli.json"):
-    logger = app_config.app_logger
-    logger.info("Evaluating using file: {}".format(filename))
-    file_path = join(app_config.resources_path, filename)
-    with open(file_path, "r") as f:
-        data = json.load(f)
-    documents = data["data"]["documents"]
-    if documents:
-        arg_mining = ArgumentMining(app_config=app_config)
-        for document in documents:
-            document = utils.get_initial_json(document["name"], document["text"])
-            arg_mining.predict(document=document)
+    arg_mining.run_pipeline()
 
 
 def main():
@@ -110,7 +66,6 @@ def main():
         try:
             app_config.elastic_save.stop()
             app_config.elastic_retrieve.stop()
-            # esclient_swo.stop()
         except(BaseException, Exception):
             app_config.app_logger.error("Could not close ssh tunnels")
             exit(-1)
