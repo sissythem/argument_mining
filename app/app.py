bac7f2d2f84d69c9aa1636ebcdbe0cd941b4753b
sthemeli <c.themeli@gmail.com>
2021-07-19 18:44:19 +0300

    add request json params

--- app/app.py
+++ app/app.py
@@ -1,15 +1,21 @@
 import json
 from os import getcwd
 from os.path import join
+from typing import AnyStr, List, Union
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 from src.utils.config import AppConfig
 
 app = FastAPI()
 config = AppConfig()
 logger = config.app_logger
+
+
+class PipelineRequest(BaseModel):
+    links: Union[List[AnyStr], AnyStr]
 
 
 @app.on_event("startup")
@@ -24,8 +30,15 @@ def hello_world():
 
 
 @app.post('/predict')
-def predict():
-    # title = request.args.get("title")
+def predict(pipeline_request: PipelineRequest):
+    links = pipeline_request.links
+    if not links:
+        raise HTTPException(status_code=500, detail="Missing URLs in request body for downloading")
+    if type(links) == str or type(links) == bytes:
+        links = [links]
+    links = [link.decode('utf8') if type(link) == bytes else link for link in links]
+
+    # TODO remove demo code below
     app_path = getcwd()
     if app_path.endswith("mining"):
         app_path = join(app_path, "app")
